92b3de4d93e986016bdd7f24d93d2c342a1ee07a
REBUG2416 <davidagochukwu@gmail.com>
2025-09-22 15:35:47 +0100

    MULTIPLE COMPONENT IMPLEMENTATIONS

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+//@ts-ignore
+import { defineConfig } from "vite";
+//@ts-ignore
+import react from "@vitejs/plugin-react";
+//@ts-ignore
+import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
